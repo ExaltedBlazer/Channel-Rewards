9d040492ffbea6d1322b9721b109649875db88d1
Kaden Horrell-St.Cloud <death5819@gmail.com>
2024-12-31 13:51:41 -0500

    Corrected script in javascript  for Channel-Points

--- script/index.js
+++ script/index.js
@@ -1,1 +1,14 @@
 console.log(`=============== sanity check for index.js ===============`)
+const divContainer = document.getElementById('testDiv');
+const indexButton = document.querySelectorAll('.div-index-button');
+console.log(divContainer);
+[...indexButton].forEach(function (row, i) {
+    if (i % 2 === 0) { row.style.backgroundColor = 'blue'; }
+});
+
+
+divContainer.addEventListener('click', clickIndexButton);
+
+function clickIndexButton(e) {
+    alert('place Script tag at the bottom of the page')
+}
